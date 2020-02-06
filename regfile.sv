e095a03b0f2f1468392f2943a5f74d90d4150dd1
Zavosh <zmottahedeh@cs.stonybrook.edu>
2020-02-05 20:50:15 -0500

    regfile is done

--- regfile.sv
+++ regfile.sv
@@ -10,6 +10,14 @@ module RegFile
     output [63:0] out1,
     output [63:0] out2
 );
+    reg [63:0] regs [0:31];
+
+    assign out1 = read_addr1 != 5'h00 ? regs[read_addr1] : 64'h0000_0000_0000_0000;
+    assign out2 = read_addr2 != 5'h00 ? regs[read_addr2] : 64'h0000_0000_0000_0000;
+
+    always @(posedge clk) begin
+        if (wb_en) regs[wb_addr] = wb_data;
+    end
 
 endmodule
 
