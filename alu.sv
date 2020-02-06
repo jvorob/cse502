c2f46cfda36991fb5203fce5e30cc3237b3707ca
Daniel Mu <dmu@cs.stonybrook.edu>
2020-02-05 20:06:52 -0500

    Created files for alu and regfile

---
+++ alu.sv
@@ -0,0 +1,13 @@
+
+module Alu
+(
+    input [63:0] a,             // rs1
+    input [63:0] b,             // rs2 or immediate
+    input [2:0] func3,
+    input [6:0] func7,
+    input [6:0] op,
+    output [63:0] result
+);
+
+endmodule
+
---
+++ regfile.sv
@@ -0,0 +1,15 @@
+
+module RegFile
+(
+    input clk,
+    input [4:0] read_addr1,
+    input [4:0] read_addr2,
+    input [4:0] wb_addr,
+    input [63:0] wb_data,
+    input wb_en,
+    output [63:0] out1,
+    output [63:0] out2
+);
+
+endmodule
+
