c695c75b098dbc1e4e37e71bf87f72dedc7415a2
Zavosh <zmottahedeh@cs.stonybrook.edu>
2020-02-16 03:29:07 -0500

    one-line icache is added

---
+++ icache.sv
@@ -0,0 +1,91 @@
+module Icache
+#(
+    ID_WIDTH = 13,
+    ADDR_WIDTH = 64,
+    DATA_WIDTH = 64
+)
+(
+    input clk,
+    input reset,
+    
+    // Pipeline interface
+    input  [63:0]   sm_pc,
+    output [31:0]   ir,
+    output          icache_valid,
+
+    // AXI interface
+    output  reg  [ID_WIDTH-1:0]    m_axi_arid,
+    output  reg  [ADDR_WIDTH-1:0]  m_axi_araddr,
+    output  reg  [7:0]             m_axi_arlen,
+    output  reg  [2:0]             m_axi_arsize,
+    output  reg  [1:0]             m_axi_arburst,
+    output  reg                    m_axi_arlock,
+    output  reg  [3:0]             m_axi_arcache,
+    output  reg  [2:0]             m_axi_arprot,
+    output  wire                   m_axi_arvalid,
+    input   wire                   m_axi_arready,
+    input   wire [ID_WIDTH-1:0]    m_axi_rid,
+    input   wire [DATA_WIDTH-1:0]  m_axi_rdata,
+    input   wire [1:0]             m_axi_rresp,
+    input   wire                   m_axi_rlast,
+    input   wire                   m_axi_rvalid,
+    output  wire                   m_axi_rready
+);
+
+    localparam LOG_WORD_LEN = 3; // log(number of bytes in word)
+    localparam LINE_LEN = 8; // number of words in line
+    localparam LOG_LINE_LEN = 3; // log(number of words in line)
+
+    reg [DATA_WIDTH-1:0] mem [LINE_LEN];
+    reg [ADDR_WIDTH-1:LOG_LINE_LEN+LOG_WORD_LEN] line_tag;
+    reg line_valid;
+    
+    reg [2:0] state;
+    reg [LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_WORD_LEN] offset;
+
+    assign ir = sm_pc[LOG_WORD_LEN-1] ? mem[sm_pc[LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_WORD_LEN]][63:32] : mem[sm_pc[LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_WORD_LEN]][31:0];
+    assign icache_valid = sm_pc[ADDR_WIDTH-1:LOG_LINE_LEN+LOG_WORD_LEN] == line_tag && line_valid;
+    assign m_axi_arvalid = state == 3'h1;
+    assign m_axi_rready = state == 3'h2;
+
+    always_ff @ (posedge clk) begin
+        if (reset) begin
+            state <= 3'h0;
+            line_valid <= 1'b0;
+            
+            m_axi_arid <= 0;      // transaction id
+            m_axi_araddr <= 0;    // address
+            m_axi_arlen <= 8'h7;  // +1, =8 words requested
+            m_axi_arsize <= 3'h3; // 2^3, word width is 8 bytes
+            m_axi_arburst <= 2'h2;// 2 in enum, bursttype=wrap
+            m_axi_arlock <= 1'b0; // no lock
+            m_axi_arcache <= 4'h0;// no cache
+            m_axi_arprot <= 3'h6; // enum, means something
+        end else begin
+            case(state)
+            3'h0: begin  // idle
+                // It's addressed by bytes, even though you don't get full granularity at byte level
+                m_axi_araddr <= sm_pc;
+                if(!icache_valid)
+                    state <= 3'h1;
+            end
+            3'h1: begin // address channel
+                line_tag <= m_axi_araddr[ADDR_WIDTH-1:LOG_LINE_LEN+LOG_WORD_LEN];
+                line_valid <= 1'b0;
+                offset <= m_axi_araddr[LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_WORD_LEN];
+                if(m_axi_arready)
+                    state <= 3'h2;
+            end
+            3'h2: begin // data channel
+                if(m_axi_rvalid)
+                    offset <= offset + 1;
+                if(m_axi_rlast) begin
+                    line_valid <= 1'b1;
+                    state <= 3'h0;
+                end
+            end
+            default: state <= 3'h0;
+            endcase
+        end
+    end
+endmodule
--- top.sv
+++ top.sv
@@ -5,6 +5,7 @@
 `include "regfile.sv"
 `include "pipe_reg.sv"
 `include "hazard.sv"
+`include "icache.sv"
 
 module top
 #(
@@ -50,7 +51,7 @@ module top
   output  reg                    m_axi_arlock,
   output  reg  [3:0]             m_axi_arcache,
   output  reg  [2:0]             m_axi_arprot,
-  output  reg                    m_axi_arvalid,
+  output  wire                   m_axi_arvalid,
   input   wire                   m_axi_arready,
   input   wire [ID_WIDTH-1:0]    m_axi_rid,
   input   wire [DATA_WIDTH-1:0]  m_axi_rdata,
@@ -64,13 +65,13 @@ module top
   input   wire [3:0]             m_axi_acsnoop
 );
 
-    logic [2:0] state;
     logic [63:0] sm_pc; //PC of axi-fetching state machine
-    logic [63:0] ir;
+    logic [31:0] ir;
+    logic icache_valid;
 
     // Curr instruction and PC going to decoder
-    logic [63:0] pc;
-    logic [31:0] cur_inst;
+    logic [63:0] pc = sm_pc;
+    logic [31:0] cur_inst = ir;
 
     logic enable_execute; //set by state machine, is high for one clock for each instr
     // until we have instruction cache, many clock cycles spent on AXI-fetch
@@ -79,8 +80,7 @@ module top
 
     // ------------------------BEGIN IF STAGE--------------------------
 
-
-
+    Icache icache (.*);
 
     // ------------------------END IF STAGE----------------------------
 
@@ -218,90 +218,25 @@ module top
     );
 
 
+    assign enable_execute = icache_valid;
 
-
-    // === Run until we hit a 0x0000_0000 instruction
     always_ff @ (posedge clk) begin
-        if (cur_inst == 0 && !reset) begin 
-            $display("===== Program terminated =====");
-            $display("    PC = 0x%0x", pc);
-            for(int i = 0; i < 32; i++)
-                $display("    r%2.2d: %10d (0x%x)", i, rf.regs[i], rf.regs[i]);
+        if (reset)
+            sm_pc <= entry;
+        else if (icache_valid) begin
+            if (ir == 0) begin // === Run until we hit a 0x0000_0000 instruction
+                $display("===== Program terminated =====");
+                $display("    PC = 0x%0x", pc);
+                    for(int i = 0; i < 32; i++)
+                    $display("    r%2.2d: %10d (0x%x)", i, rf.regs[i], rf.regs[i]);
             
 
-            $finish;
+                $finish;
+            end
+            else
+                sm_pc <= sm_pc + 64'h4;
         end
     end
-
-    // === Main state machine
-
-    always_ff @ (posedge clk) begin
-        if (reset) begin
-            cur_inst <= -1; // we exit when this is 0000, so start at FFFF
-
-            state <= 3'h0;
-            sm_pc <= entry;
-
-            enable_execute <= 0;
-
-            m_axi_arid <= 0;      // master id
-            m_axi_arlen <= 8'h7;  // +1, =8 words requested
-            m_axi_arsize <= 3'h3; // 2^3, word width is 8 bytes
-            m_axi_arburst <= 2'h2;// 2 in enum, bursttype=wrap
-            m_axi_arlock <= 1'b0; // no lock
-            m_axi_arcache <= 4'h0;// no cache
-            m_axi_arprot <= 3'h6; // enum, means something
-            m_axi_arvalid <= 1'b0;// signal
-            m_axi_rready <= 1'b0; // signal
-        end else begin
-            case(state)
-            3'h0: begin  // Start Read
-
-                if(!m_axi_arready || !m_axi_arvalid) begin
-                    // It's addressed by bytes, even though you don't get full granularity at byte level
-                    m_axi_araddr <= sm_pc[63:0];
-                    m_axi_arvalid <= 1'b1;
-                end else begin
-                    sm_pc <= sm_pc + 64'h8;
-                    m_axi_rready <= 1'b1;
-                    m_axi_arvalid <= 1'b0;
-                    state <= 3'h1;
-                end
-
-                enable_execute <= 0;
-            end
-            3'h1: begin // Address Accepted / Awaiting Read Valid
-                if(m_axi_rvalid) begin
-                    ir <= m_axi_rdata;
-                    state <= 3'h2;
-                end
-            end
-            3'h2: begin // Wait for remaining blocks to be sent
-                if(m_axi_rlast) begin
-                    m_axi_rready <= 1'b0;
-                    state <= 3'h3;
-                end
-            end
-            3'h3: begin // Read done, decode low
-                cur_inst <= ir[31:0];
-                pc <= sm_pc - 8;
-                state <= 3'h4;
-                enable_execute <= 1; //UGH this is really gross, 
-                // we should move to the state-machine style where all effects
-                // are combinational, based on the current state, not latched
-                // at state transitions. TODO --Jan
-            end
-            3'h4: begin // Decode hi
-                cur_inst <= ir[63:32];
-                pc <= sm_pc - 4;
-                state <= 3'h0;
-                enable_execute <= 1;
-            end
-            default: state <= 3'h0;
-            endcase
-        end
-    end
-    
 
   initial begin
         $display("Initializing top, entry point = 0x%x", entry);
