bdc3c5e54d1ac82e9d3dba430a38d6a0ff0d5033
zavosh <zavosh76@yahoo.com>
2020-03-23 01:58:30 -0400

    dcache is changed to 4-way

--- dcache.sv
+++ dcache.sv
@@ -1,3 +1,5 @@
+`include "lru.sv"
+
 module Dcache
 #(
     ID_WIDTH = 13,
@@ -10,14 +12,14 @@ module Dcache
     input  reset,
     
     // Pipeline interface
-    input  [63:0]   addr,
-    input  [63:0]   wdata,
-    input  [ 1:0]   wlen, // len = 2 ^ wlen bytes
-    input           dcache_enable,
-    input           wrn, // write = 1 / read = 0
-    output [63:0]   rdata,
-    output          dcache_valid,
-    output          write_done,
+    input        [63:0]   addr,
+    input        [63:0]   wdata,
+    input        [ 1:0]   wlen, // len = 2 ^ wlen bytes
+    input                 dcache_enable,
+    input                 wrn, // write = 1 / read = 0
+    output  reg  [63:0]   rdata,
+    output  reg           dcache_valid,
+    output  reg           write_done,
 
     // AXI interface
     output  reg  [ID_WIDTH-1:0]     dcache_m_axi_awid,
@@ -66,34 +68,59 @@ module Dcache
     localparam LINE_LEN = 8; // number of words in line
     localparam LOG_LINE_LEN = 3; // log(number of words in line)
     localparam SIZE = 16 * 1024; // size of cache in bytes
-    localparam WAYS = 1; // direct map
+    localparam WAYS = 4; // 4-way
     localparam SETS = SIZE / (WAYS * LINE_LEN * WORD_LEN); // number of sets in cache
-    localparam LOG_SETS = 8; // log(number of sets in cache)
+    localparam LOG_SETS = 6; // log(number of sets in cache)
+    localparam LRU_LEN = 5; // 5 bit is enough for 4-way
 
     reg [DATA_WIDTH-1:0] mem [SETS][WAYS][LINE_LEN];
     reg [ADDR_WIDTH-1:LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN] line_tag [SETS][WAYS];
     reg line_valid [SETS][WAYS];
     reg line_dirty [SETS][WAYS];
+    reg [LRU_LEN-1:0] line_lru [SETS];
     
     reg [2:0] state;
     reg [63:0] rplc_addr;
     reg [LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_WORD_LEN] rplc_offset;
     wire [LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_LINE_LEN+LOG_WORD_LEN] rplc_index = rplc_addr[LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_LINE_LEN+LOG_WORD_LEN];
     wire [ADDR_WIDTH-1:LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN] rplc_tag = rplc_addr[ADDR_WIDTH-1:LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN];
+    wire [1:0] victim_way = !line_valid[index][0] ? 2'h0 : !line_valid[index][1] ? 2'h1 : !line_valid[index][2] ? 2'h2 : !line_valid[index][3] ? 2'h3 : line_lru[index][1:0];
+    reg [1:0] rplc_way;
 
     wire [LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_LINE_LEN+LOG_WORD_LEN] snoop_index = dcache_m_axi_acaddr[LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_LINE_LEN+LOG_WORD_LEN];
     wire [ADDR_WIDTH-1:LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN] snoop_tag = dcache_m_axi_acaddr[ADDR_WIDTH-1:LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN];
+    integer snoop_way;
 
     wire [LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_WORD_LEN] offset = addr[LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_WORD_LEN];
     wire [LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_LINE_LEN+LOG_WORD_LEN] index = addr[LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_LINE_LEN+LOG_WORD_LEN];
     wire [ADDR_WIDTH-1:LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN] tag = addr[ADDR_WIDTH-1:LOG_SETS+LOG_LINE_LEN+LOG_WORD_LEN];
 
-    assign rdata = mem[index][0][offset];
-    assign dcache_valid = !dcache_m_axi_acvalid && dcache_enable && !wrn && tag == line_tag[index][0] && line_valid[index][0];
-    assign write_done = state == 3'h0 && !dcache_m_axi_acvalid && dcache_enable && wrn && tag == line_tag[index][0] && line_valid[index][0];
+    integer way;
+    integer mru;
+    always_comb begin
+        rdata = mem[index][0][offset];
+        dcache_valid = 1'b0;
+        write_done = 1'b0;
+        mru = 0;
+        for (way = 0; way < WAYS; way = way + 1) 
+            if (tag == line_tag[index][way] && line_valid[index][way]) begin
+                rdata = mem[index][way][offset];
+                dcache_valid = !dcache_m_axi_acvalid && dcache_enable && !wrn;
+                write_done = state == 3'h0 && !dcache_m_axi_acvalid && dcache_enable && wrn;
+                mru = way;
+            end
+    end
+
+    always_ff @ (posedge clk) begin
+        if (reset)
+            line_lru <= '{SETS{5'b0_01_00}}; // 3 2 1 0
+        else if (dcache_valid || write_done)
+            line_lru[index] <= new_lru(line_lru[index], mru);
+    end
+
     assign dcache_m_axi_araddr = {rplc_addr[ADDR_WIDTH-1:LOG_WORD_LEN], {LOG_WORD_LEN{1'b0}}};
-    assign dcache_m_axi_awaddr = {line_tag[rplc_index][0], rplc_index, {LOG_LINE_LEN{1'b0}}, {LOG_WORD_LEN{1'b0}}};
-    assign dcache_m_axi_wdata = mem[rplc_index][0][rplc_offset];
+    assign dcache_m_axi_awaddr = {line_tag[rplc_index][rplc_way], rplc_index, {LOG_LINE_LEN{1'b0}}, {LOG_WORD_LEN{1'b0}}};
+    assign dcache_m_axi_wdata = mem[rplc_index][rplc_way][rplc_offset];
     assign dcache_m_axi_acready = state == 3'h0;
     assign dcache_m_axi_awvalid = state == 3'h1;
     assign dcache_m_axi_wvalid = state == 3'h2;
@@ -128,19 +155,21 @@ module Dcache
             case(state)
             3'h0: begin // idle
                 if(dcache_m_axi_acvalid && (dcache_m_axi_acsnoop == 4'hd)) begin // snoop invalidation
-                    if(line_tag[snoop_index][0] == snoop_tag) begin
-                        line_valid[snoop_index][0] <= 1'b0;
-                        line_dirty[snoop_index][0] <= 1'b0;
-                    end
+                    for (snoop_way = 0; snoop_way < WAYS; snoop_way = snoop_way + 1)
+                        if(line_tag[snoop_index][snoop_way] == snoop_tag) begin
+                            line_valid[snoop_index][snoop_way] <= 1'b0;
+                            line_dirty[snoop_index][snoop_way] <= 1'b0;
+                        end
                 end else if(dcache_enable) begin
                     rplc_addr <= addr;
-                    if(tag == line_tag[index][0] && line_valid[index][0]) begin // hit
-                        if(wrn) begin // write
+                    rplc_way <= victim_way;
+                    if(dcache_valid || write_done) begin // hit
+                        if(write_done) begin // write
                             case(wlen)
-                            2'h0: mem[index][0][offset][8*addr[LOG_WORD_LEN-1:0]+:8] <= wdata;
-                            2'h1: mem[index][0][offset][16*addr[LOG_WORD_LEN-1:1]+:16] <= wdata;
-                            2'h2: mem[index][0][offset][32*addr[LOG_WORD_LEN-1]+:32] <= wdata;
-                            2'h3: mem[index][0][offset] <= wdata;
+                            2'h0: mem[index][mru][offset][8*addr[LOG_WORD_LEN-1:0]+:8] <= wdata;
+                            2'h1: mem[index][mru][offset][16*addr[LOG_WORD_LEN-1:1]+:16] <= wdata;
+                            2'h2: mem[index][mru][offset][32*addr[LOG_WORD_LEN-1]+:32] <= wdata;
+                            2'h3: mem[index][mru][offset] <= wdata;
                             endcase
 
                             // Also notify do_pending_write to make Mike's ecall hacks work 
@@ -151,9 +180,9 @@ module Dcache
                             2'h3: do_pending_write({addr[ADDR_WIDTH-1:3], 3'b000}, wdata, 8);
                             endcase
 
-                            line_dirty[index][0] <= 1'b1;
+                            line_dirty[index][mru] <= 1'b1;
                         end
-                    end else if(line_valid[index][0] && line_dirty[index][0]) // miss needs write back
+                    end else if(line_valid[index][victim_way] && line_dirty[index][victim_way]) // miss needs write back
                         state <= 3'h1;
                     else // miss no need to write back
                         state <= 3'h3;
@@ -170,25 +199,25 @@ module Dcache
                     //$display("dcache write-back offset: %x, data: %x", rplc_offset, dcache_m_axi_wdata);
                     rplc_offset <= rplc_offset + 1;
                     if(dcache_m_axi_wlast) begin
-                        line_dirty[rplc_index][0] <= 1'b0;
+                        line_dirty[rplc_index][rplc_way] <= 1'b0;
                         state <= 3'h3;
                     end
                 end
             end
             3'h3: begin // address channel
                 //$display("dcache fetch request addr: %x", dcache_m_axi_araddr);
-                line_tag[rplc_index][0] <= rplc_tag;
-                line_valid[rplc_index][0] <= 1'b0;
+                line_tag[rplc_index][rplc_way] <= rplc_tag;
+                line_valid[rplc_index][rplc_way] <= 1'b0;
                 rplc_offset <= rplc_addr[LOG_LINE_LEN+LOG_WORD_LEN-1:LOG_WORD_LEN];
                 if(dcache_m_axi_arready)
                     state <= 3'h4;
             end
             3'h4: begin // data channel
                 if(dcache_m_axi_rvalid) begin
-                    mem[rplc_index][0][rplc_offset] <= dcache_m_axi_rdata;
+                    mem[rplc_index][rplc_way][rplc_offset] <= dcache_m_axi_rdata;
                     rplc_offset <= rplc_offset + 1;
                     if(dcache_m_axi_rlast) begin
-                        line_valid[rplc_index][0] <= 1'b1;
+                        line_valid[rplc_index][rplc_way] <= 1'b1;
                         state <= 3'h0;
                     end
                 end
